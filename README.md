b1be04414300af36f4fda883d045e331716e58ec
Pedro Nícolas Vieira <pedronicolas576@gmail.com>
2024-07-30 04:12:45 -0300

    First Upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Lista de Tarefas
+
+Este é um projeto simples de uma lista de tarefas (To-Do List) desenvolvida com HTML, CSS e JavaScript. O objetivo é permitir que os usuários adicionem, visualizem e removam tarefas, com as tarefas sendo armazenadas no Local Storage do navegador para persistência.
+
+## Funcionalidades
+
+- Adicionar novas tarefas.
+- Visualizar a lista de tarefas em tempo real.
+- Remover tarefas da lista.
+- Verificação de duplicatas (case-insensitive).
+- Persistência das tarefas no Local Storage do navegador.
+
+## Como Usar
+
+- Escreva sua tarefa que deseja concluir e clique em adicionar tarefa.
+- A tarefa será adicionada à lista e caso não queira mais a tarefa você pode removela.
+
+- Você pode acessar projeto através do site [Task To Do](https://task-to-do-pnv.netlify.app)
+
+### Estrutura do Projeto
+
+- `index.html`: Estrutura HTML da aplicação.
+- `style.css`: Estilos CSS para a aplicação.
+- `main.js`: Funcionalidades JavaScript para a aplicação.
---
+++ main.js
@@ -0,0 +1,71 @@
+// Elementos do DOM
+const taskForm = document.getElementById('task-form');
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('task-list');
+
+// Tarefas do Local Storage
+function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(task => {
+        createTaskElement(task);
+    });
+}
+
+// Novo elemento de tarefa
+function createTaskElement(task) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(task));
+
+    const removeBtn = document.createElement('button');
+    removeBtn.appendChild(document.createTextNode('Remover'));
+    li.appendChild(removeBtn);
+
+    taskList.appendChild(li);
+
+    // Remove 
+    removeBtn.addEventListener('click', removeTask);
+}
+
+// Adicionar nova tarefa
+function addTask(e) {
+    e.preventDefault();
+
+    const newTask = taskInput.value.trim();
+
+    // Verificalção de repetição    
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    if (tasks.map(task => task.toLowerCase()).includes(newTask.toLowerCase())) {
+        alert('Tarefa já adicionada!');
+        taskInput.value = '';
+        return;
+    }
+
+    createTaskElement(newTask);
+    saveTask(newTask);
+    taskInput.value = '';
+}
+
+// Salvar no Local Storage
+function saveTask(task) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remover uma tarefa
+function removeTask(e) {
+    const item = e.target.parentElement;
+    taskList.removeChild(item);
+    removeTaskFromLocalStorage(item.firstChild.textContent);
+}
+function removeTaskFromLocalStorage(task) {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    const filteredTasks = tasks.filter(t => t.toLowerCase() !== task.toLowerCase());
+    localStorage.setItem('tasks', JSON.stringify(filteredTasks));
+}
+
+// Evento de submit ao formulário
+taskForm.addEventListener('submit', addTask);
+
+// Carregando tarefas
+document.addEventListener('DOMContentLoaded', loadTasks);
